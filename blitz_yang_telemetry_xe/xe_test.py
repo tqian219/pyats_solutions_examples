122d54f3fa1d2cc3fba6acda22c454b237ae9e37
Tom Qian <tqian219@gmail.com>
2023-11-10 17:27:58 -0500

    blitz update

--- blitz_yang_telemetry_xe/xe_test.py
+++ blitz_yang_telemetry_xe/xe_test.py
@@ -9,16 +9,13 @@ from genie.harness.main import gRun
 def main():
     trigger_uids = [
         'native_oper_interface_statistics_get',
-        'openconfig_gnmi_fan_statistics_subscribe',
         'native_netconf_cpu_statistics_subscribe',
-        'nexus_openconfig_gnmi_fan_statistics_subscribe',
-        'nexus_netconf_interface_state_subscribe',
     ]
 
     gRun(trigger_uids=trigger_uids,
-         trigger_datafile="test_trigger.yaml",
-         mapping_datafile="mapping.yaml",
-         subsection_datafile="subsection.yaml")
+         trigger_datafile="xe_trigger.yaml",
+         mapping_datafile="xe_mapping.yaml",
+         subsection_datafile="xe_subsection.yaml")
 
 
 if __name__ == '__main__':
